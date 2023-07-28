122f35bc46cd51e609d71d5b3206830c25de8c77
vaibhavprashanth <65724191+vaibhavprashanth@users.noreply.github.com>
2023-07-28 14:09:00 +0530

    Added audio output. Commit by Vaibhav

--- hand_gesture_recognition/hand_gesture_detection.py
+++ hand_gesture_recognition/hand_gesture_detection.py
@@ -1,12 +1,11 @@
-# import necessary packages
-
 import cv2
 import numpy as np
 import mediapipe as mp
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+import pyttsx3
 
-# initialize mediapipe
+# Initialize mediapipe
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
@@ -20,9 +19,11 @@ classNames = f.read().split('\n')
 f.close()
 print(classNames)
 
-
 # Initialize the webcam
 cap = cv2.VideoCapture(0)
+
+# Initialize the text-to-speech engine
+engine = pyttsx3.init()
 
 while True:
     # Read each frame from the webcam
@@ -37,8 +38,6 @@ while True:
     # Get hand landmark prediction
     result = hands.process(framergb)
 
-    # print(result)
-    
     className = ''
 
     # post process the result
@@ -46,7 +45,6 @@ while True:
         landmarks = []
         for handslms in result.multi_hand_landmarks:
             for lm in handslms.landmark:
-                # print(id, lm)
                 lmx = int(lm.x * x)
                 lmy = int(lm.y * y)
 
@@ -57,21 +55,24 @@ while True:
 
             # Predict gesture
             prediction = model.predict([landmarks])
-            # print(prediction)
             classID = np.argmax(prediction)
             className = classNames[classID]
 
     # show the prediction on the frame
-    cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
-                   1, (0,0,255), 2, cv2.LINE_AA)
+    cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                1, (0, 0, 255), 2, cv2.LINE_AA)
 
     # Show the final output
-    cv2.imshow("Output", frame) 
+    cv2.imshow("Output", frame)
 
-    if cv2.waitKey(1) == ord('q'):
+    # Check for 's' key press and provide audio output of the current hand gesture
+    key = cv2.waitKey(1)
+    if key == ord('q'):
         break
+    elif key == ord('s'):
+        engine.say(f"Current hand gesture is {className}")
+        engine.runAndWait()
 
 # release the webcam and destroy all active windows
 cap.release()
-
 cv2.destroyAllWindows()
